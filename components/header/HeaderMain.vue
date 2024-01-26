139115ce61a0b1426c03aafac564b5856f668fb3
tokuyama_shota <tokush1031@gmail.com>
2024-01-26 18:29:01 +0900

    Nuxt swiper

--- components/header/HeaderMain.vue
+++ components/header/HeaderMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between p-4 items-center">
+  <header class="flex justify-between p-4 items-center mb-8">
     <HeaderTitle />
     <HeaderNavigation />
     <HeaderTheme />
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
 <script setup lang="ts"></script>
 <template>
-  <h1>Hello</h1>
+  <SwiperView />
 </template>
